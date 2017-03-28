29eb08ebf50a6fd748cdfaf614d1aea3756a82a8
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-03-28 14:24:30 +0100

    Account fillable

--- app/Account.php
+++ app/Account.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    // protected $guarded = []; opisite $fillable
     protected $fillable = ['name', 'tel', 'email','is_stu','notes'];
+
+    
 }
