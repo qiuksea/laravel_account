696604be196a3a825c47e914eddb6ff0403f7d20
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-03-30 13:59:45 +0100

    remove script from footer move to script.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -13,28 +13,7 @@
     </div>
     <!-- end: Footer sub -->
 
-    <!--: start: JS scripts -->
-    <script src="/js/uni/script.min.js"></script>
-    <!--: end: JS scripts -->
 
-  <script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
-
-  <script
-  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-  crossorigin="anonymous"></script>
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
-
-  <script>
-        jQuery(document).ready(function() {
-            @yield('postJquery');
-        });
-  </script>
 
 <!-- MGMT SCHL -->
 
