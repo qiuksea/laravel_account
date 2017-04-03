2547d36767f246f66acb645730b62060b5bc8340
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-04-03 11:30:55 +0100

    add edit/patch/delete routes, add sample all routes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/*
+
+GET /posts -index
+GET /posts/create - create
+POST /posts -store
+GET /posts/{id}/edit - edit 
+GET /posts/{id} -show
+PATCH /posts/{id} - update
+DELETE /posts/{id} - destroy
+
+*/
+
+
 Route::get('/', 'AccountsController@index');
 
 Route::get('/accounts', 'AccountsController@index');
 
-#Route::get('/accounts/{account}', 'AccountsController@show');
+Route::get('/accounts/{account}', 'AccountsController@show');
 
 Route::get('/accounts/create', 'AccountsController@create');
 
 Route::post('/accounts', 'AccountsController@store');
+
+Route::get('/accounts/{account}/edit', 'AccountsController@edit');
+
+Route::patch('/accounts/{account}', 'AccountsController@update');
+
+Route::delete('/accounts/{account}', 'AccountsController@destroy');
 
