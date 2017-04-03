ff1a3e41713bf6ebbf6ef0bc8ac6445b4428de7e
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-04-03 11:30:14 +0100

    remove bootstrap as it has been include by laravel

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -12,8 +12,6 @@
   integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
   crossorigin="anonymous"></script>
 
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
 
   <script>
         jQuery(document).ready(function() {
