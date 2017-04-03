78a31cbc8c25a673f6d88b1b11b8c7962ce8e65e
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-04-03 11:29:48 +0100

    update show fields

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -3,8 +3,27 @@
 @section ('content')
 
 <div class="col-md-8 blog-main">
-
- 		{{  $account ->email  }}
+<table class="table table-striped table-bordered">
+  <tbody>
+    <tr  class="table-info">
+      <th>Detail</th>
+      <th></th>
+    </tr>
+    <tr>
+      <td>Name</td>
+      <td>{{  $account ->name  }}</td>
+    </tr>
+    <tr>
+      <td>Tel: </td>
+      <td>{{  $account ->tel  }}</td>
+    </tr>
+    <tr>
+      <td>Email: </td>
+      <td>{{  $account ->email  }}</td>
+    </tr>
+  </tbody>
+</table>
+ 		
 </div>
 @endsection
 
