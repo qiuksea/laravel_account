d1756345c898b9008266e06d9216e5ae11f3916e
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-04-26 10:50:58 +0100

    add accounts seeder

---
+++ database/seeds/AccountsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AccountsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        for ($i=0; $i < 8; $i++) { 
+	    	DB::table('accounts')->insert([
+	            'name' => str_random(8),
+	            'tel' => str_random(8),
+	            'email' => str_random(5).'@mail.com',
+	            'notes' => 'test',
+	            'company_project_10k' => 0
+
+	        ]);
+    	}
+    }
+}
