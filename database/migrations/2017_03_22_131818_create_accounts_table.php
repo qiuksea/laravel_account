28644dcf45a9ba5d5e4af41bcf578cbf632aaf33
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-06-01 10:10:06 +0100

    add 'status' in table

--- database/migrations/2017_03_22_131818_create_accounts_table.php
+++ database/migrations/2017_03_22_131818_create_accounts_table.php
@@ -46,6 +46,9 @@ class CreateAccountsTable extends Migration
             $table->string('company_eligibility'); //company_eligibility
             $table->string('company_eligibility_other'); //company_eligibility_other
             $table->text('company_notes'); //company_comments
+
+            $table->string('status'); //setup
+
             $table->timestamps();
         });
     }
