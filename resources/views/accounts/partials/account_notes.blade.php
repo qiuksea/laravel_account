b1c19d75d1f9d3b8e3590bb4ae751ff51136fe96
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-06-13 14:48:47 +0100

    account-> notes function, can orderBy

--- resources/views/accounts/partials/account_notes.blade.php
+++ resources/views/accounts/partials/account_notes.blade.php
@@ -1,5 +1,27 @@
+
+
+<div class="card">
+	<div class="card-block">
+		<form method="POST" action="/accounts/{{$account->id}}/notes">
+
+			{{ csrf_field()}}
+			
+			<div class="form-group">
+			  	<label for="note"><strong>Add Note</strong></label>
+	    		<textarea class="form-control" id="content" name="content" required></textarea>
+			</div>
+			<div class="form-group">
+			    <button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+
+		</form>		
+	</div>
+</div>
+
+<hr>
+
 <ul class="list-group">
-	 	@foreach( $account-> notes as $note)
+	 	@foreach( $account-> allNotes()->get() as $note)
 
 		  <li class="list-group-item">
 
