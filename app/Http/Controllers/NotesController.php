94fc85e9463ee790dad309a196ff429064f72bd9
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-06-13 14:44:47 +0100

    Add in: notes -> store

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -15,25 +15,23 @@ class NotesController extends Controller
 {
    
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+   
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Account $account)
     {
         //
+
+        $this->validate(request(), ['content' => 'required|min:2']);
+
+        $account->addNote(request('content'));
+    
+
+        return back();
     }
 
     /**
