127ec4c1b566ca3b1292047a3366fdaf5c000dfc
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-11-30 11:48:51 +0000

    publish xavrsl/cas

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        //XavRsl/Cas
+        Xavrsl\Cas\CasServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Helper' => App\Helpers\Helper::class,
+        'Cas' => Xavrsl\Cas\Facades\Cas::class,
 
 
     ],
