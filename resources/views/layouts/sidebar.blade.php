a393e2f4d495dba11e45698e8985b0a8b22bb37f
Jingyu Qi <j.qi@sheffield.ac.uk>
2017-12-05 11:37:38 +0000

    fix bug for rejected

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,6 @@
 <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar pt-5">
-          
           <ul class="nav nav-pills flex-column">
-	           <li class="nav-item">		    
+          	  <li class="nav-item">		    
 			  	<a class="nav-link active" href="/accounts">All</a>			
 			  </li>
 			  <li class="nav-item">
@@ -17,9 +16,9 @@
 			    <a class="nav-link" href="/accounts?status=Y">Complete Requests</a>
 			  </li>
 			  <li class="nav-item">
-			    <a class="nav-link" href="{{ route('accounts', ['status' => 'R']) }}">Rejected Requests</a>
+			    <a class="nav-link" href="/accounts?status=R">Rejected Requests</a>
 			  </li>
-          </ul>
+          </ul> 
 
           <ul class="nav nav-pills flex-column">
             @yield ('sidebar_2')        
