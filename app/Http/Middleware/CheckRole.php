3eaa3665dd0be7f579062d447fa63838bef6cbfc
Qi <j.qi@sheffield.ac.uk>
2018-01-16 13:26:16 +0000

    tidy up view

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -35,7 +35,8 @@ class CheckRole
 
             } else {
 
-                return response()->view('/dashboard'); #https://laracasts.com/discuss/channels/general-discussion/disable-global-verifycsrftoken-and-use-as-middleware-doesnt-work-setcookie-on-null?page=1   
+                return response()->view('/dashboard'); 
+                #https://laracasts.com/discuss/channels/general-discussion/disable-global-verifycsrftoken-and-use-as-middleware-doesnt-work-setcookie-on-null?page=1   
 
             } //if ($user)
 
