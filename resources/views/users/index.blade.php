8ade68a694f618f52878569b1e851c5edc012b4c
Qi <j.qi@sheffield.ac.uk>
2018-01-16 13:27:51 +0000

    update user view for: modal-window, update, delete

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,14 +1,72 @@
 @extends ('layouts.uni')
 
-@section ('content')			
+@section ('content')
 
-                 
-	<div class="col-md-10">		
+	<div class="col-md-10">	
+		<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addUser">
+		Add User
+	  	</button>
+	  
+	  <!-- Modal -->
+	  <div class="modal fade" id="addUser" tabindex="-1" role="dialog" 
+	  aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+		  <div class="modal-content">
+
+			<form method="POST" action="/users">				
+
+			<div class="modal-header">
+			  <h5 class="modal-title" id="AddUserTitle">Add Admin Form</h5>
+			  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			  </button>
+			</div>
+					<div class="modal-body">
+							
+								{{csrf_field() }}
+								
+								<div class="form-group row">
+									<label class="col-md-2 col-form-label" for="email">Email</label>
+									<div class="col-md-10">
+										<input type="email" class="form-control col-md-6" name="email" id="email" aria-describedby="emailHelp">
+									</div>					  
+								</div>
+			
+								<div class="form-group row">
+									<div class="col-md-2">Role</div>
+									<div class="col-md-10">
+										<div class="form-check">
+												<input class="form-check-input" type="radio" name="role_id" id="role1" value="1" checked>
+												<label class="form-check-label" for="role1">
+														Admin
+												</label>
+										</div>
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="role_id" id="role2" value="2">
+											<label class="form-check-label" for="role2">
+													Super Admin
+											</label>
+										</div>
+									</div>	
+								</div>
+						
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<div class="col-md-10">
+							<button type="submit" class="btn btn-primary">Add</button>
+						</div>		
+					</div>
+
+					</form>
+		  </div>
+		</div>
+		</div>
+			
+		<hr>
 		
-			<a href="/accounts/create" class="btn btn-primary">New User Form</a>
-			<hr>
-
-		    <table class="table table-hover table-bordered">
+		<table class="table table-hover table-bordered">
 			  <thead class="table-info">
 			    <tr>
 			    	  <th>ID</th>
@@ -17,36 +75,59 @@
 				      <th>Status</th>
 				      <th>Created at</th>
 				      <th>Updated at</th>
-				      <th>Action</th>
+							<th colspan="2">Action</th>
 			    </tr>
 			  </thead>
 			  <tbody>
-			    @foreach ($users as $user)
+					@foreach ($users as $user)
+						<tr id="user_{{$user->id}}">
+								<td>
+									{{ $user ->id}}
+								</td>
+							 <td>{{ $user ->email}} </td>
 
-					<tr id="user_{{$user->id}}">
-				    	 <td>
-				    		{{ $user ->id}}
-				    	 </td>
-						 <td>{{ $user ->email}} </td>
-						 
-					     <td>{{ $user ->role -> name }}</td>
+							 <form method="POST" action="/users/{{$user->id}}">
+			
+								{{csrf_field() }}
+		
+								{{ method_field('PATCH') }}
+								<td>
+									<input type="radio" name="role_id" value="1" {{ $user->hasRole('Admin') ? 'checked' : ''}} >Admin 
+									<input type="radio" name="role_id" value="2" {{ $user->hasRole('Super Admin') ? 'checked' : ''}} >Super Admin
+									</td>
 
-					     <td>{{ $user ->disabled }}</td>
-					
-					     <td>{{ $user ->created_at -> toFormattedDateString()  }}</td>
-						 <td>{{ $user ->updated_at -> toFormattedDateString()  }}</td>
-						 					     
-					     <td>
-						     <a href="/users/{{ $user-> id }}/edit" class="btn btn-primary btn-sm">Edit</a> |
-									      
-							 <button class="btn btn-danger btn-sm delete-account" value="{{$user->id}}">Delete</button>
-					     </td>
-			    	</tr>
+								<td>{{ $user ->disabled }}</td>
+						
+								<td>{{ $user ->created_at -> toFormattedDateString()  }}</td>
+							  <td>{{ $user ->updated_at -> toFormattedDateString()  }}</td>
+														
+								<td>
+									<button type="submit" class="btn btn-primary btn-sm">Edit Role</button>
+								</td>
+							</form>
+								<td>	
+									<form method="POST" action="/users/{{$user->id}}">
+										{{csrf_field() }}	
+
+										{{ method_field('DELETE') }}
+
+										<button type="submit" 
+									
+										class="btn btn-danger btn-sm">Delete User</button>
+
+									</form>								
+
+								</td>
+
+							</tr>
+
+				
 
 		    	@endforeach
 			   
 			  </tbody>
 			</table>
+
 	</div>
 
 @endsection
