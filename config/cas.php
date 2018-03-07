544d82191703b82e2ef62bf20c81d03d8ad8be1b
qiuksea <qijingyu@hotmail.com>
2018-03-07 15:51:26 +0000

    Update cas.php

--- config/cas.php
+++ config/cas.php
@@ -89,7 +89,7 @@ return [
         |
         */
 
-        'cas_pretend_user' => env('CAS_PRETEND_USER', 'ad1jqi'), #give user id to pretend already login, e.g. ad1sec, ad1ehlp
+        'cas_pretend_user' => env('CAS_PRETEND_USER', 'my user id'), #give user id to pretend already login, 
 
         /*
         |--------------------------------------------------------------------------
