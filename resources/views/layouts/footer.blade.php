8c91de11e97a9d284921ac695dea1bd6340025f0
Qi <j.qi@sheffield.ac.uk>
2018-03-21 15:13:38 +0000

    add footer style

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,5 @@
-    <footer class="footer">
+    <footer class="footer text-center text-small">
       <div class="container">
         <span class="text-muted">&copy; Department of Finance, The University of Sheffield</span>
       </div>
     </footer>
-
-
-
-<!-- MGMT SCHL -->
-
