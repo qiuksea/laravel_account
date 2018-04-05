78e71e3692d7a41cf00468a2b15840fbdb466ac1
Qi <j.qi@sheffield.ac.uk>
2018-04-05 15:04:09 +0100

    update style for notes

--- resources/views/accounts/partials/account_notes.blade.php
+++ resources/views/accounts/partials/account_notes.blade.php
@@ -1,7 +1,5 @@
-
-
-<div class="card">
-	<div class="card-block">
+<div class="card border-primary mb-3">
+	<div class="card-body">
 		<form method="POST" action="/accounts/{{$account->id}}/notes">
 
 			{{ csrf_field()}}
@@ -9,10 +7,9 @@
 			<div class="form-group">
 			  	<label for="note"><strong>Add Note</strong></label>
 	    		<textarea class="form-control" id="content" name="content" required></textarea>
-			</div>
-			<div class="form-group">
-			    <button type="submit" class="btn btn-primary">Submit</button>
-			</div>
+			</div>	
+
+			<button type="submit" class="btn btn-primary">Submit</button>	
 
 		</form>		
 	</div>
@@ -20,15 +17,20 @@
 
 <hr>
 
-<ul class="list-group">
-	 	@foreach( $account-> allNotes()->get() as $note)
+<div class="card border-info mb-3">
+	<div class="card-body">
+		<ul class="list-group list-group-flush">
+				@foreach( $account-> allNotes()->get() as $note)
 
-		  <li class="list-group-item">
+				<li class="list-group-item">
 
-		  	{{ $note -> created_at  }}  by  note use name here <br>
+					{{ $note -> created_at  }}  by  note use name here <br>
 
-		  	{{  $note->content }}
+					{{  $note->content }}
 
-		  </li>
-	  	@endforeach
-</ul>
+				</li>
+				@endforeach
+		</ul>
+	</div>
+</div>
+
