190afd85669041f5d5bfafa24f4b48f81354b553
Qi <j.qi@sheffield.ac.uk>
2018-04-05 14:00:37 +0100

    remove space

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,28 +10,26 @@
                   All 
                 </a>
               </li>
-							<li class="nav-item">
-								<a class="nav-link"	href="/accounts?status=N"><span data-feather="file"></span> New Requests</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="/accounts?status=P"><span data-feather="watch"></span> Pending Requests</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="/accounts?status=A"><span data-feather="credit-card"></span> Await credit check</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="/accounts?status=Y"><span data-feather="thumbs-up"></span> Complete Requests</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="/accounts?status=R"><span data-feather="thumbs-down"></span> Rejected Requests</a>
-							</li>
-			 		@endif 
+				<li class="nav-item">
+					<a class="nav-link"	href="/accounts?status=N"><span data-feather="file"></span> New Requests</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/accounts?status=P"><span data-feather="watch"></span> Pending Requests</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/accounts?status=A"><span data-feather="credit-card"></span> Await credit check</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/accounts?status=Y"><span data-feather="thumbs-up"></span> Complete Requests</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/accounts?status=R"><span data-feather="thumbs-down"></span> Rejected Requests</a>
+				</li>
+			@endif 
 
 			</ul> 				
 
-<div>Route: {{ Route::current()->getName()}}</div>
-
-			
+			<div>Route: {{ Route::current()->getName()}}</div>			
 					
 			@if (Auth::user() && Auth::user()->role->id == 2)
 
@@ -39,13 +37,14 @@
 
 			<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
 			</span>Super Admin Only</span>
-      </h6>
+      		</h6>
 
-	          <ul class="nav flex-column mb-2">
-	             <li class="nav-item">
-				    <a class="nav-link" href="/users"><span data-feather="users"></span>Users Management</a> 
-				 </li>       
-	          </ul> 
+			<ul class="nav flex-column mb-2">
+				<li class="nav-item">
+				<a class="nav-link" href="/users"><span data-feather="users"></span>Users Management</a> 
+				</li>       
+			</ul> 
+
 			@endif 
 
 	</div>   
