8aabfd4f8e52e537795935153eeeb43d981a2dae
Qi <j.qi@sheffield.ac.uk>
2018-04-05 15:20:28 +0100

    update style for status - using boostrap-4:card

--- resources/views/accounts/partials/account_status.blade.php
+++ resources/views/accounts/partials/account_status.blade.php
@@ -1,51 +1,55 @@
+<div class="card border-primary mb-3">
+  <div class="card-header">
+    Status: <strong>{{ Helper::display_status($account->status) }} </strong>
+  </div>
+  <div class="card-body text-primary" style="width: 22rem;">
+	<h5 class="card-title">Update Status</h5>
+	<div class="card-text">
+		<form method="POST" action="/accounts/{{ $account-> id }}/status">
 
-<div class="col-md-8">
+			{{csrf_field() }} 
 
-	<form method="POST" action="/accounts/{{ $account-> id }}/status">
+			{{ method_field('PATCH') }}
 
-	 	{{csrf_field() }} 
+			<div class="form-group">
+				<select class="form-control" id="status" name="status">
+					<option value="N"
+					{{ 
+					Helper::display_dropdown_selected('N', old('status'), $account->status)
+							}}
 
-		{{ method_field('PATCH') }}
+					> New </option>
+					<option value="P"
+						{{ 
+					Helper::display_dropdown_selected('P', old('status'), $account->status)
+							}}> Pending </option>
+					<option value="A"
+						{{ 
+					Helper::display_dropdown_selected('A', old('status'), $account->status)
+							}}> Await credit check </option>
+					<option value="Y"
+						{{ 
+					Helper::display_dropdown_selected('Y', old('status'), $account->status)
+							}}> Complete </option>
+					<option value="R"
+						{{ 
+					Helper::display_dropdown_selected('R', old('status'), $account->status)
+							}}> Rejected </option>
 
-	 	<div class="form-group">
-		      <select class="form-control" id="status" name="status">
-		          <option value="N"
-		           {{ 
-		           Helper::display_dropdown_selected('N', old('status'), $account->status)
-		                }}
+				</select>
+			</div>
+		
+			<button type="submit" class="btn btn-primary">Submit</button>
 
-		          > New </option>
-				  <option value="P"
-				     {{ 
-		           Helper::display_dropdown_selected('P', old('status'), $account->status)
-		                }}> Pending </option>
-				  <option value="A"
-				     {{ 
-		           Helper::display_dropdown_selected('A', old('status'), $account->status)
-		                }}> Await credit check </option>
-				  <option value="Y"
-				     {{ 
-		           Helper::display_dropdown_selected('Y', old('status'), $account->status)
-		                }}> Complete </option>
-				  <option value="R"
-				     {{ 
-		           Helper::display_dropdown_selected('R', old('status'), $account->status)
-		                }}> Rejected </option>
-
-		      </select>
-	  	</div>
-
-	  	<div class="form-group">
-		    <button type="submit" class="btn btn-primary">Submit</button>
-		</div>
-
-	</form>
+		</form>
+	</div>
+  </div>
 </div>
 
-<hr>
+	
 
-<div class="col-md-10">
-	<p>Current Status: <strong>{{ Helper::display_status($account->status) }} </strong> </p>
-	<hr>
-</div>
+
+
+
+
 
